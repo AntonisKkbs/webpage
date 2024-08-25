41610a3460077592ab3697e004f8abe5a22af1b5
Antonis K. <116433694+AntonisKkbs@users.noreply.github.com>
2024-08-25 18:05:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
             window.location.href = 'academy.html'; // Redirect to academy.html
         });
     }
-
-    // Hide the PDF viewer when the "Escape" key is pressed (if needed elsewhere)
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape') {
-            // Optional: Hide PDF viewer if it's implemented in other scenarios
-        }
-    });
 });
