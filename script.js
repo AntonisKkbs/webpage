23e35741d8df529fd256955c19781589eb0020cb
Antonis K. <116433694+AntonisKkbs@users.noreply.github.com>
2024-08-25 17:39:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const ethicalHackingButton = document.querySelector('#ethicalHackingBtn');
     const pdfViewer = document.querySelector('#pdfViewer');
     
+    // Function to toggle the PDF viewer
+    function togglePdfViewer() {
+        if (pdfViewer.style.display === 'block') {
+            pdfViewer.style.display = 'none'; // Hide the PDF viewer
+        } else {
+            pdfViewer.style.display = 'block'; // Show the PDF viewer
+        }
+    }
+
     if (ethicalHackingButton) {
         ethicalHackingButton.addEventListener('click', (event) => {
             event.preventDefault(); // Prevent the default link behavior
-            pdfViewer.style.display = 'block'; // Show the PDF viewer
+            togglePdfViewer(); // Toggle the PDF viewer
         });
     }
+
+    // Hide the PDF viewer when the "Escape" key is pressed
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            pdfViewer.style.display = 'none'; // Hide the PDF viewer
+        }
+    });
 });
